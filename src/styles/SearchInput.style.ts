fbafe5a982fcd5bb32adcb4cc3b1812db8e57230
thumbthing <ilscc0902@gmail.com>
2023-09-06 19:48:01 +0900

    Feat: style 파일 생성

---
+++ src/styles/SearchInput.style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Button = styled.button``;
+
+export const Input = styled.input``;
+
+// 컨테이너 스타일
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  max-width: 400px;
+  min-height: 50px;
+  margin: 100px 100px 100px 100px;
+`;
+
+export const ListContainer = styled.div``;
---
+++ src/styles/SuggestedSearchTermList.style.ts
@@ -0,0 +1,9 @@
+import { styled } from 'styled-components';
+
+export const ListContainer = styled.div``;
+
+export const TermList = styled.ul``;
+
+export const TermItem = styled.li``;
+
+export const SickName = styled.span``;
