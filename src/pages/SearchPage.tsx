cc4a3ea7aafa95ea946cbc126f6fb6a1d35ef374
thumbthing <ilscc0902@gmail.com>
2023-09-06 18:17:35 +0900

    Feat: 이전 Session Storage 초기화

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SearchInput from '../components/SearchInput';
 import SuggestedSearchTermList from '../components/SuggestedSearchTermList';
 import { SearchProvider } from '../context/SearchContext';
 import { FunctionContext } from '../context/FunctionContext';
 
 function SearchPage() {
+  // 캐싱 되어 있는 데이터 초기화
+
+  useEffect(() => {
+    sessionStorage.clear();
+  }, []);
   return (
     <SearchProvider>
       <FunctionContext>
