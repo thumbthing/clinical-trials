16742d2ae43a161ec9502dfda4b16c54417ec2c7
thumbthing <ilscc0902@gmail.com>
2023-09-06 18:08:42 +0900

    Feat: Session Storage 데이터 파싱 기능 선언

--- src/utils/sessionHandler.ts
+++ src/utils/sessionHandler.ts
@@ -1,7 +1,7 @@
 import { SickArray } from '../types/context.type';
 import handleError from './errorHandler';
 
-const sessionHandler = async (inputText: string, termResult: SickArray[]) => {
+export const sessionHandler = async (inputText: string, termResult: SickArray[]) => {
   try {
     const serializedData = JSON.stringify(termResult);
     sessionStorage.setItem(inputText, serializedData);
@@ -10,4 +10,16 @@ const sessionHandler = async (inputText: string, termResult: SickArray[]) => {
   }
 };
 
-export default sessionHandler;
+export const sessionParser = async (input: string): Promise<SickArray[] | null> => {
+  try {
+    const cachedData: string | null = sessionStorage.getItem(input);
+    if (typeof cachedData === 'string') {
+      const formatedData = JSON.parse(cachedData);
+      return formatedData;
+    }
+    return null;
+  } catch (error) {
+    handleError(error);
+    return null;
+  }
+};
