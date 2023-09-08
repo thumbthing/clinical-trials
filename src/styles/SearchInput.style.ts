e562641f3c7b9048dfc97fd87d1ee34d04292db9
thumbthing <ilscc0902@gmail.com>
2023-09-08 13:18:59 +0900

    Design: 검색창 스타일링 추가

--- src/styles/SearchInput.style.ts
+++ src/styles/SearchInput.style.ts
@@ -1,17 +1,43 @@
 import styled from 'styled-components';
 
-export const Button = styled.button``;
-
-export const Input = styled.input``;
+export const SearchContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  min-width: 150px;
+  min-height: 150px;
+  flex: 1;
+`;
 
 // 컨테이너 스타일
 export const Container = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
   flex-direction: row;
-  justify-content: space-between;
-  max-width: 400px;
-  min-height: 50px;
-  margin: 100px 100px 100px 100px;
+  position: relative;
+  min-height: 4rem;
+  min-width: 20rem;
+  border: 1px solid;
+  border-radius: 1.2rem;
+  margin-top: 1rem;
 `;
 
-export const ListContainer = styled.div``;
+export const Button = styled.button`
+  font-size: 1.2rem;
+  border-radius: 1.2rem;
+  max-height: 1.2rem;
+  max-width: 1.2rem;
+`;
+
+export const Input = styled.input`
+  font-size: 1.2rem;
+  min-width: 1.2rem;
+  min-height: 1.2rem;
+  width: 100%;
+`;
+
+export const SearchInputListBox = styled.div`
+  position: relative;
+`;
--- src/styles/SuggestedSearchTermList.style.ts
+++ src/styles/SuggestedSearchTermList.style.ts
@@ -1,9 +1,41 @@
 import { styled } from 'styled-components';
 
-export const ListContainer = styled.div``;
+export const ListContainer = styled.div`
+  display: flex;
+  border: 1px solid;
+  justify-content: center;
+  flex-direction: column;
 
-export const TermList = styled.ul``;
+  border-top: none;
+  border-bottom-left-radius: 1rem;
+  border-bottom-right-radius: 1rem;
+  position: absolute;
+  top: 100%;
+  overflow-y: auto;
+  width: 100%;
+`;
 
-export const TermItem = styled.li``;
+export const TermList = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  min-height: 1.2rem;
+  margin: 0px 0px 0px 0px;
+  padding: 0px;
+  flex-grow: auto;
+`;
+
+export const TermItem = styled.li`
+  display: flex;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  margin: 1px;
+  padding: 1px;
+  flex: 1;
+  border-top: 1px solid rgba(0, 0, 0, 0.2);
+  font-size: 1.2rem;
+  background-color: white;
+`;
 
 export const SickName = styled.span``;
